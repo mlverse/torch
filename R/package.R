41ec2002f6d137e233a126ad1f0eedcace2f73a8
Daniel Falbel <dfalbel@gmail.com>
2020-01-09 19:16:03 -0300

    more cleanups

---
+++ R/package.R
@@ -0,0 +1,3 @@
+#' @useDynLib torch
+#' @importFrom Rcpp sourceCpp
+NULL
--- R/utils.R
+++
@@ -1,9 +0,0 @@
-args_to_pointers <- function(args) {
-  lapply(args, function(x) {
-
-    if (inherits(x, "tensor"))
-      return(x$pointer)
-
-    x
-  })
-}
