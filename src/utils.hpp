120ff58165316d7f293725f360665f92d4527b32
Daniel Falbel <dfalbel@gmail.com>
2020-02-06 18:20:21 -0300

    convert vector to arrays

--- src/utils.hpp
+++ src/utils.hpp
@@ -5,3 +5,10 @@ Rcpp::XPtr<type> make_xptr  (type x) {
   auto * out = new type(x);
   return Rcpp::XPtr<type>(out);
 }
+
+template <class type, int n>
+std::array<type, n> std_vector_to_std_array (std::vector<type> x) {
+  std::array<type,n> out;
+  std::copy_n(x.begin(), n, out.begin());
+  return out;
+}
