dff825fadce8ab42c0b45f490924828963c4a079
Javier Luraschi <javierluraschi@hotmail.com>
2020-02-06 15:49:10 -0800

    test with dynamic loading and windows tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,8 +1,14 @@
-#define LANTERN_BUILD
+#include <iostream>
 #include "lantern/lantern.h"
 
 int main(int argc, char *argv[])
 {
+    std::string error;
+    if (!lanternInit(argv[1], &error)) {
+        std::cout << "Error: " << error;
+        return 1;
+    }
+
     lanternPrint();
 
     return 0;
