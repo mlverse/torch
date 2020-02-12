852871acc78f4719c783e10a4a3e7692550cf56b
Javier Luraschi <javierluraschi@hotmail.com>
2020-02-12 13:49:36 -0800

    simplify test client

--- tests/main.cpp
+++ tests/main.cpp
@@ -3,22 +3,8 @@
 
 #include "lantern/lantern.h"
 
-inline bool exists(const std::string& path) {
-    if (FILE *file = fopen(path.c_str(), "r")) {
-        fclose(file);
-        return true;
-    } else {
-        return false;
-    }   
-}
-
 int main(int argc, char *argv[])
 {
-    if (argc < 2 || !exists(argv[1])) {
-        std::cout << "Error: Incorrect path provided";
-        return 1;
-    }
-
     std::string error;
     if (!lanternInit(argv[1], &error)) {
         std::cout << "Error: " << error;
