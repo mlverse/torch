b503a708803d0863f85c5d35775a255e7845fc39
Javier Luraschi <javierluraschi@hotmail.com>
2020-02-13 16:06:14 -0800

    load yaml declarations in gen project

--- headers/src/main.cpp
+++ headers/src/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+#include "yaml-cpp/yaml.h"
+
 int main(int argc, char *argv[])
 {
+    if (argc < 2) {
+        std::cout << "Usage: lanterngen declarations.yaml" << std::endl;
+        return 1;
+    }
+    
+    YAML::Node config = YAML::LoadFile(argv[1]);
+
     return 0;
 }
