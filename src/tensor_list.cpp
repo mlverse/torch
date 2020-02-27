4a4a95a4368a180cac553a4c0de2eabdc6f7e326
Daniel Falbel <dfalbel@gmail.com>
2020-02-27 09:02:48 -0300

    use lantern for tensor list

--- src/tensor_list.cpp
+++ src/tensor_list.cpp
@@ -2,27 +2,30 @@
 #include "utils.hpp"
 
 // [[Rcpp::export]]
-Rcpp::XPtr<std::vector<torch::Tensor>> cpp_torch_tensor_list (const Rcpp::List & x) {
-  std::vector<torch::Tensor> out;
-  
-  for (int i = 0; i < x.length(); i++) {
-    torch::Tensor tmp = *Rcpp::as<Rcpp::XPtr<torch::Tensor>>(x[i]);
-    out.push_back(tmp);
+Rcpp::XPtr<XPtrTorch> cpp_torch_tensor_list(const Rcpp::List &x)
+{
+  auto out = lantern_TensorList();
+
+  for (int i = 0; i < x.length(); i++)
+  {
+    lantern_TensorList_push_back(out, Rcpp::as<Rcpp::XPtr<XPtrTorch>>(x[i])->get());
   }
-  
-  return make_xptr<std::vector<torch::Tensor>>(out);
+
+  return make_xptr<XPtrTorch>(out);
 }
 
 // [[Rcpp::export]]
-Rcpp::List cpp_tensor_list_to_r_list (Rcpp::XPtr<std::vector<torch::Tensor>> x) {
-  
+Rcpp::List cpp_tensor_list_to_r_list(Rcpp::XPtr<XPtrTorch> x)
+{
+
   Rcpp::List out;
-  auto y = *x;
-  
-  for (int i = 0; i < x->size(); i ++) {
-    auto tmp = make_xptr<torch::Tensor>(y.at(i));
-    out.push_back(tmp);
+  int64_t sze = lantern_TensorList_size(x->get());
+
+  for (int i = 0; i < sze; i++)
+  {
+    XPtrTorch tmp = lantern_TensorList_at(x->get(), i);
+    out.push_back(make_xptr<XPtrTorch>(tmp));
   }
-  
+
   return out;
 }
