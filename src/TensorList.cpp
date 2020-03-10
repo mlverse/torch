16e5574733929fe6630f1d08847153aa6df21ac5
Daniel Falbel <dfalbel@gmail.com>
2020-03-10 13:32:19 -0300

    fix the tensor list implementation to use LanternObjects

--- src/TensorList.cpp
+++ src/TensorList.cpp
@@ -8,24 +8,27 @@
 
 #include "utils.hpp"
 
-void* lantern_TensorList () {
-    auto out = new std::vector<torch::Tensor>;
-    return (void*) out;
+void *lantern_TensorList()
+{
+    return (void *)new LanternPtr<std::vector<torch::Tensor>>();
 }
 
-void lantern_TensorList_push_back(void* self, void* x) {
-    auto slf = reinterpret_cast<std::vector<torch::Tensor>*>(self);
-    torch::Tensor ten = reinterpret_cast<LanternObject<torch::Tensor>*>(x)->get();
-    slf->push_back(ten);
+void lantern_TensorList_push_back(void *self, void *x)
+{
+    auto slf = reinterpret_cast<LanternObject<std::vector<torch::Tensor>> *>(self)->get();
+    torch::Tensor ten = reinterpret_cast<LanternObject<torch::Tensor> *>(x)->get();
+    slf.push_back(ten);
 }
 
-void* lantern_TensorList_at (void* self, int64_t i) {
-    auto slf = reinterpret_cast<std::vector<torch::Tensor>*>(self);
-    torch::Tensor out = slf->at(i);
-    return (void*) new LanternObject<torch::Tensor>(out);
+void *lantern_TensorList_at(void *self, int64_t i)
+{
+    auto slf = reinterpret_cast<LanternObject<std::vector<torch::Tensor>> *>(self)->get();
+    torch::Tensor out = slf.at(i);
+    return (void *)new LanternObject<torch::Tensor>(out);
 }
 
-int64_t lantern_TensorList_size (void* self) {
-    auto slf = reinterpret_cast<std::vector<torch::Tensor>*>(self);
-    return slf->size();
+int64_t lantern_TensorList_size(void *self)
+{
+    auto slf = reinterpret_cast<LanternObject<std::vector<torch::Tensor>> *>(self)->get();
+    return slf.size();
 }
