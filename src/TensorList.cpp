f737ecf790cbb94d033530f7b6a9844a9d6114f0
Daniel Falbel <dfalbel@gmail.com>
2020-03-10 13:58:49 -0300

    oneliner and LanternPtr -> LanternObject

--- src/TensorList.cpp
+++ src/TensorList.cpp
@@ -10,25 +10,22 @@
 
 void *lantern_TensorList()
 {
-    return (void *)new LanternPtr<std::vector<torch::Tensor>>();
+    return (void *)new LanternObject<std::vector<torch::Tensor>>();
 }
 
 void lantern_TensorList_push_back(void *self, void *x)
 {
-    auto slf = reinterpret_cast<LanternObject<std::vector<torch::Tensor>> *>(self)->get();
     torch::Tensor ten = reinterpret_cast<LanternObject<torch::Tensor> *>(x)->get();
-    slf.push_back(ten);
+    reinterpret_cast<LanternObject<std::vector<torch::Tensor>> *>(self)->get().push_back(ten);
 }
 
 void *lantern_TensorList_at(void *self, int64_t i)
 {
-    auto slf = reinterpret_cast<LanternObject<std::vector<torch::Tensor>> *>(self)->get();
-    torch::Tensor out = slf.at(i);
+    torch::Tensor out = reinterpret_cast<LanternObject<std::vector<torch::Tensor>> *>(self)->get().at(i);
     return (void *)new LanternObject<torch::Tensor>(out);
 }
 
 int64_t lantern_TensorList_size(void *self)
 {
-    auto slf = reinterpret_cast<LanternObject<std::vector<torch::Tensor>> *>(self)->get();
-    return slf.size();
+    return reinterpret_cast<LanternObject<std::vector<torch::Tensor>> *>(self)->get().size();
 }
