56354fd170fa706a9c18d84b78acb9c30fe40186
Daniel Falbel <dfalbel@gmail.com>
2020-03-12 14:38:45 -0300

    add deleters for other kinds of objects

--- src/Delete.cpp
+++ src/Delete.cpp
@@ -8,7 +8,48 @@
 
 #include "utils.hpp"
 
+template <class T>
+void lantern_delete(void *x)
+{
+  delete reinterpret_cast<LanternObject<T> *>(x);
+}
+
 void lantern_Tensor_delete(void *x)
 {
-  delete reinterpret_cast<LanternObject<torch::Tensor> *>(x);
+  lantern_delete<torch::Tensor>(x);
+}
+
+void lantern_bool_delete(void *x)
+{
+  lantern_delete<bool>(x);
+}
+
+void lantern_int64_t_delete(void *x)
+{
+  lantern_delete<int64_t>(x);
+}
+
+void lantern_TensorList_delete(void *x)
+{
+  lantern_delete<std::vector<torch::Tensor>>(x);
+}
+
+void lantern_double_delete(void *x)
+{
+  lantern_delete<double>(x);
+}
+
+void lantern_QScheme_delete(void *x)
+{
+  lantern_delete<torch::QScheme>(x);
+}
+
+void lantern_Scalar_delete(void *x)
+{
+  lantern_delete<torch::Scalar>(x);
+}
+
+void lantern_ScalarType_delete(void *x)
+{
+  lantern_delete<torch::ScalarType>(x);
 }
