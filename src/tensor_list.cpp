9650cfaa62edf73aebaa120d8311d31d3653d93c
Daniel Falbel <dfalbel@gmail.com>
2020-03-13 15:35:45 -0300

    correctly manage memory for TensorLists

--- src/tensor_list.cpp
+++ src/tensor_list.cpp
@@ -4,11 +4,11 @@
 // [[Rcpp::export]]
 Rcpp::XPtr<XPtrTorchTensorList> cpp_torch_tensor_list(const Rcpp::List &x)
 {
-  void * out = lantern_TensorList();
+  XPtrTorchTensorList out = lantern_TensorList();
 
   for (int i = 0; i < x.length(); i++)
   {
-    lantern_TensorList_push_back(out, Rcpp::as<Rcpp::XPtr<XPtrTorch>>(x[i])->get());
+    lantern_TensorList_push_back(out.get(), Rcpp::as<Rcpp::XPtr<XPtrTorch>>(x[i])->get());
   }
 
   return make_xptr<XPtrTorchTensorList>(out);
