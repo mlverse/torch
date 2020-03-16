585d3a9d66737d51fb2c931f2d732133b93ca54f
Daniel Falbel <dfalbel@gmail.com>
2020-03-16 09:47:33 -0300

    use shared pointer for generators

--- src/Generator.cpp
+++ src/Generator.cpp
@@ -8,15 +8,18 @@
 
 #include "utils.hpp"
 
-void* lantern_Generator () {
-  auto out = new at::CPUGenerator();
-  return (void *) new LanternObject<torch::Generator*>(out);
+void *lantern_Generator()
+{
+  std::shared_ptr<torch::Generator> out = std::shared_ptr<torch::Generator>(new at::CPUGenerator());
+  return (void *)new LanternObject<std::shared_ptr<torch::Generator>>(out);
 }
 
-uint64_t lantern_Generator_current_seed (void* generator) {
-  return reinterpret_cast<LanternObject<torch::Generator*>*>(generator)->get()->current_seed();
+uint64_t lantern_Generator_current_seed(void *generator)
+{
+  return reinterpret_cast<LanternObject<std::shared_ptr<torch::Generator>> *>(generator)->get()->current_seed();
 }
 
-void lantern_Generator_set_current_seed (void* generator, uint64_t seed) {
-  reinterpret_cast<LanternObject<torch::Generator*>*>(generator)->get()->set_current_seed(seed);
+void lantern_Generator_set_current_seed(void *generator, uint64_t seed)
+{
+  reinterpret_cast<LanternObject<std::shared_ptr<torch::Generator>> *>(generator)->get()->set_current_seed(seed);
 }
