1f6f7812e1b4d21fb69a79e456d78b6ac3128034
Daniel Falbel <dfalbel@gmail.com>
2020-03-16 10:25:19 -0300

    add deleters for dimnames and dimnamelists

--- src/Delete.cpp
+++ src/Delete.cpp
@@ -11,70 +11,80 @@
 template <class T>
 void lantern_delete(void *x)
 {
-  delete reinterpret_cast<LanternObject<T> *>(x);
+  delete reinterpret_cast<T *>(x);
 }
 
 void lantern_Tensor_delete(void *x)
 {
-  lantern_delete<torch::Tensor>(x);
+  lantern_delete<LanternObject<torch::Tensor>>(x);
 }
 
 void lantern_bool_delete(void *x)
 {
-  lantern_delete<bool>(x);
+  lantern_delete<LanternObject<bool>>(x);
 }
 
 void lantern_int64_t_delete(void *x)
 {
-  lantern_delete<int64_t>(x);
+  lantern_delete<LanternObject<int64_t>>(x);
 }
 
 void lantern_TensorList_delete(void *x)
 {
-  lantern_delete<std::vector<torch::Tensor>>(x);
+  lantern_delete<LanternObject<std::vector<torch::Tensor>>>(x);
 }
 
 void lantern_double_delete(void *x)
 {
-  lantern_delete<double>(x);
+  lantern_delete<LanternObject<double>>(x);
 }
 
 void lantern_QScheme_delete(void *x)
 {
-  lantern_delete<torch::QScheme>(x);
+  lantern_delete<LanternObject<torch::QScheme>>(x);
 }
 
 void lantern_Scalar_delete(void *x)
 {
-  lantern_delete<torch::Scalar>(x);
+  lantern_delete<LanternObject<torch::Scalar>>(x);
 }
 
 void lantern_ScalarType_delete(void *x)
 {
-  lantern_delete<torch::ScalarType>(x);
+  lantern_delete<LanternObject<torch::ScalarType>>(x);
 }
 
 void lantern_TensorOptions_delete(void *x)
 {
-  lantern_delete<torch::TensorOptions>(x);
+  lantern_delete<LanternObject<torch::TensorOptions>>(x);
 }
 
 void lantern_Dtype_delete(void *x)
 {
-  lantern_delete<torch::Dtype>(x);
+  lantern_delete<LanternObject<torch::Dtype>>(x);
 }
 
 void lantern_Device_delete(void *x)
 {
-  lantern_delete<torch::Device>(x);
+  lantern_delete<LanternObject<torch::Device>>(x);
 }
 
 void lantern_Layout_delete(void *x)
 {
-  lantern_delete<torch::Layout>(x);
+  lantern_delete<LanternObject<torch::Layout>>(x);
 }
 
 void lantern_Generator_delete(void *x)
 {
-  lantern_delete<std::shared_ptr<torch::Generator>>(x);
+  lantern_delete<LanternObject<std::shared_ptr<torch::Generator>>>(x);
+}
+
+void lantern_Dimname_delete(void *x)
+{
+  lantern_delete<LanternPtr<torch::Dimname>>(x);
+}
+
+void lantern_DimnameList_delete(void *x)
+{
+  lantern_delete<LanternPtr<std::vector<torch::Dimname>>>(x);
 }
