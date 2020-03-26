8dfcc01c61dd273377e885180f15b7cb0546d09b
Daniel Falbel <dfalbel@gmail.com>
2020-03-26 14:38:19 -0300

    test for empty tensors

--- tests/testthat/test-tensor.R
+++ tests/testthat/test-tensor.R
@@ -6,6 +6,9 @@ test_that("Can create a tensor", {
   x <- torch_tensor(1, dtype = torch_double())
   
   expect_s3_class(x, "torch_tensor")
+  
+  x <- torch_tensor(numeric(), dtype = torch_float32())
+  expect_equal(dim(x), 0)
 })
 
 test_that("Numeric tensors", {
