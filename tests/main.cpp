9a9da3d93e4801df07a8f44beae668a40efb6c48
Daniel Falbel <dfalbel@gmail.com>
2020-04-07 08:01:37 -0300

    remove testing code

--- tests/main.cpp
+++ tests/main.cpp
@@ -57,9 +57,6 @@ int main(int argc, char *argv[])
     void *t1 = lantern_rand_intarrayref_tensoroptions(lantern_vector_int64_t(&x[0], 2), lantern_TensorOptions());
     void *t2 = lantern_zeros_like_tensor_memoryformat(t1, nullptr);
 
-    lantern_test_register_hook();
-    test_hooks();
-
     std::cout << "Success!!" << std::endl;
     return 0;
 }
