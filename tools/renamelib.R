d5cdd2e01d9e0156213692e7cc5730b931945b3c
Javier Luraschi <javierluraschi@hotmail.com>
2020-06-03 13:43:44 -0700

    generalize lib rename to linux annd osx

--- tools/renamelib.R
+++ tools/renamelib.R
@@ -1,6 +1,11 @@
-current_path <- "C:/Users/Administrator/code/torch/src"
+libs_path <- getwd()
 
-exports_path <- normalizePath(file.path(current_path, "..", "R", "RcppExports.R"))
+for (lib in dir(libs_path, pattern = "torch.")) {
+  file.copy(file.path(libs_path, lib), file.path(libs_path, gsub("torch", "torchpkg", lib)))
+  unlink(file.path(libs_path, lib))
+}
+
+exports_path <- normalizePath(file.path(libs_path, "..", "R", "RcppExports.R"))
 exports_content <- readLines(exports_path)
 
 exports_content <- gsub("PACKAGE = 'torch'", "PACKAGE = 'torchpkg'", exports_content)
