b407e3f44aba51c9d70e069027172834f31bd080
Javier Luraschi <javierluraschi@hotmail.com>
2020-06-19 13:03:06 -0700

    automatically download torch when package loads

--- R/package.R
+++ R/package.R
@@ -6,21 +6,11 @@ NULL
 
 .onAttach <- function(libname, pkgname) {
   if (!install_exists() && interactive()) {
-    packageStartupMessage("You need to install libtorch in order to use torch.\n")
-    ans <- readline("Do you want to download it now? ~100Mb (yes/no)")
-    if (ans == "yes" | ans == "y")
-      install_torch()
-    
-    if (install_exists()) {
-      packageStartupMessage("Torch was successfully installed.")
-      packageStartupMessage("Please restart your R session now.")
-    }
-      
+    install_torch()
   }
 }
 
 .onLoad <- function(libname, pkgname){
-  
   if (!install_exists() && Sys.getenv("INSTALL_TORCH", unset = 0) == 1) {
     install_torch()
   }
