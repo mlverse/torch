762d9c323db7ae8ce9539fb51525456e0242f41f
Daniel Falbel <dfalbel@gmail.com>
2020-06-23 08:21:37 -0300

    add dataloader creation function

--- R/utils-data-dataloader.R
+++ R/utils-data-dataloader.R
@@ -3,6 +3,52 @@ length.utils_data_loader <- function(x) {
   x$.lenght()
 }
 
+#' Data loader. Combines a dataset and a sampler, and provides
+#' single- or multi-process iterators over the dataset.
+#' 
+#' @param dataset (Dataset): dataset from which to load the data.
+#' @param batch_size (int, optional): how many samples per batch to load
+#'   (default: `1`).
+#' @param shuffle (bool, optional): set to `TRUE` to have the data reshuffled
+#'   at every epoch (default: `FALSE`).
+#' @param sampler (Sampler, optional): defines the strategy to draw samples from
+#'   the dataset. If specified, `shuffle` must be False.
+#' @param batch_sampler (Sampler, optional): like sampler, but returns a batch of
+#'   indices at a time. Mutually exclusive with `batch_size`,
+#'   `shuffle`, `sampler`, and `drop_last`.
+#' @param num_workers (int, optional): how many subprocesses to use for data
+#'   loading. 0 means that the data will be loaded in the main process.
+#'   (default: `0`)
+#' @param collate_fn (callable, optional): merges a list of samples to form a mini-batch.
+#' @param pin_memory (bool, optional): If `TRUE`, the data loader will copy tensors
+#'   into CUDA pinned memory before returning them.  If your data elements
+#'   are a custom type, or your `collate_fn` returns a batch that is a custom type
+#'   see the example below.
+#' @param drop_last (bool, optional): set to `TRUE` to drop the last incomplete batch,
+#'   if the dataset size is not divisible by the batch size. If `FALSE` and
+#'   the size of dataset is not divisible by the batch size, then the last batch
+#'   will be smaller. (default: `FALSE`)
+#' @param timeout (numeric, optional): if positive, the timeout value for collecting a batch
+#'   from workers. Should always be non-negative. (default: `0`)
+#' @param worker_init_fn (callable, optional): If not `NULL`, this will be called on each
+#'   worker subprocess with the worker id (an int in `[0, num_workers - 1]`) as
+#'   input, after seeding and before data loading. (default: `NULL`)
+#'
+#' @export
+utils_dataloader <- function(dataset, batch_size = 1, shuffle = FALSE, 
+                             sampler=NULL,
+                             batch_sampler=NULL, num_workers=0, collate_fn=NULL,
+                             pin_memory=FALSE, drop_last=FALSE, timeout=0,
+                             worker_init_fn=NULL) {
+  
+  multiprocessing_context <- NULL
+  generator <- NULL
+  
+  DataLoader$new(dataset, batch_size, shuffle, sampler, batch_sampler, num_workers,
+                 collate_fn, pin_memory, drop_last, timeout, worker_init_fn, 
+                 multiprocessing_context, generator)
+}
+
 DataLoader <- R6::R6Class(
   classname = "utils_data_loader",
   lock_objects = FALSE,
