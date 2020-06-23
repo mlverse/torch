7a1f18a9698db8ede0da9817c81d57088e296d75
Daniel Falbel <dfalbel@gmail.com>
2020-06-23 08:43:37 -0300

    rename enum

---
+++ tests/testthat/test-utils-data-enum.R
@@ -0,0 +1,15 @@
+test_that("dataloader_enum", {
+  
+  x <- torch_randn(100, 100)
+  y <- torch_randn(100, 1)
+  dataset <- utils_dataset_tensor(x, y)
+  dl <- utils_dataloader(dataset = dataset, batch_size = 32)
+  
+  i <- 1
+  for (b in dataloader_enum(dl)) {
+    expect_equal_to_tensor(b[[1]], x[(1 + (i-1)*32):((i)*32),,drop=FALSE])
+    expect_equal_to_tensor(b[[2]], y[(1 + (i-1)*32):((i)*32),,drop=FALSE])  
+    i <- i + 1
+  }
+  
+})
