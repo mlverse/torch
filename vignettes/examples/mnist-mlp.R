fe0989baab8555349f6ef9e2c346362db74324b5
Daniel Falbel <dfalbel@gmail.com>
2020-06-24 00:01:31 -0300

    add mnist-mlp example

---
+++ vignettes/examples/mnist-mlp.R
@@ -0,0 +1,37 @@
+dir <- "~/Downloads/mnist"
+
+ds <- mnist_dataset(
+  dir, 
+  download = TRUE, 
+  transform = function(x) {
+    x$to(dtype = torch_float())/256
+  }
+)
+dl <- dataloader(ds, batch_size = 32, shuffle = TRUE)
+
+net <- nn_module(
+  "Net",
+  initialize = function() {
+    self$fc1 <- nn_linear(784, 128)
+    self$fc2 <- nn_linear(128, 10)
+  },
+  forward = function(x) {
+    x %>% 
+      torch_flatten(start_dim = 1) %>% 
+      self$fc1() %>% 
+      nnf_relu() %>% 
+      self$fc2() %>% 
+      nnf_log_softmax(dim = 1)
+  }
+)
+
+model <- net()
+optimizer <- optim_sgd(model$parameters, lr = 0.01)
+for (b in enumerate(dl)) {
+  optimizer$zero_grad()
+  output <- model(b[[1]])
+  loss <- nnf_nll_loss(output, b[[2]])
+  loss$backward()
+  optimizer$step()
+  print(loss)
+}
