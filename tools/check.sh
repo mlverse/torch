25634ea182946f05cef1c854d68d5e60ef55c90b
Javier Luraschi <javierluraschi@hotmail.com>
2020-06-24 20:43:27 -0700

    make gpu tests a bit faster

--- tools/check.sh
+++ tools/check.sh
@@ -6,8 +6,5 @@ apt-get install -y libcurl4-openssl-dev libssl-dev
 Rscript -e "install.packages(c('remotes', 'rcmdcheck'))"
 Rscript -e "remotes::install_deps(dependencies = TRUE)"
 
-# build lantern and torch -----
-Rscript tools/buildlantern.R
-
 # check -----------
 Rscript -e 'rcmdcheck::rcmdcheck(args = c("--no-manual", "--no-multiarch", "--no-build-vignettes"), build_args = c("--no-build-vignettes"), error_on = "error", check_dir = "check")'
