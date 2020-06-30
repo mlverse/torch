e1c5af84585819939308437ecd1915bab51efc83
Javier Luraschi <javierluraschi@hotmail.com>
2020-06-30 09:55:21 -0700

    use custom defines across lantern functions to handnle errors

--- lantern/include/lantern/lantern.h
+++ lantern/include/lantern/lantern.h
@@ -29,6 +29,15 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#define LANTERN_FUNCTION_START try {
+#define LANTERN_FUNCTION_END } catch(const std::exception& ex) { \
+  pLanternLastError = new std::string(ex.what());                \
+  return NULL;                                                   \
+} catch(...) {                                                   \
+  pLanternLastError = new std::string("Unknown error.");         \
+  return NULL;                                                   \
+}
+
 #ifdef __cplusplus
 extern "C"
 {
