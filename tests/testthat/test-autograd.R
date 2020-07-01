acc41955fef618bddbdda088d9fc7ae1ae910e49
Javier Luraschi <javierluraschi@hotmail.com>
2020-07-01 13:54:40 -0700

    uncomment fixed windows test

--- tests/testthat/test-autograd.R
+++ tests/testthat/test-autograd.R
@@ -56,7 +56,6 @@ test_that("register_hook", {
 })
 
 test_that("register hook: can throw exceptions in the lantern thread", {
-  skip_on_os("windows")
   x <- torch_tensor(c(2), requires_grad = TRUE)
   x$register_hook(function(grad) { 2* grad})
   y <- 2 * x
