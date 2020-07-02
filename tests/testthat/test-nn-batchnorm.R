fdc4943a825056f9f0216b3ccd00a10328504fda
Daniel Falbel <dfalbel@gmail.com>
2020-07-02 15:58:00 -0300

    add batchnorm2d fix #145

---
+++ tests/testthat/test-nn-batchnorm.R
@@ -0,0 +1,21 @@
+test_that("nn_batch_norm1d", {
+  x <- torch_randn(10, 10)
+  m <- nn_batch_norm1d(10, 1)
+  expect_tensor_shape(m(x), c(10, 10))
+  
+  x <- torch_randn(10, 10, 10)
+  m <- nn_batch_norm1d(10, 1)
+  expect_tensor_shape(m(x), c(10, 10, 10))
+  
+  x <- torch_randn(10, 10, 10, 10)
+  expect_error(m(x))
+})
+
+test_that("nn_batch_norm2d", {
+  m <- nn_batch_norm2d(10, 1)
+  x <- torch_randn(10, 10, 10, 10)
+  expect_tensor_shape(m(x), c(10, 10, 10, 10))
+  
+  x <- torch_randn(10, 10, 10)
+  expect_error(m(x))
+})
