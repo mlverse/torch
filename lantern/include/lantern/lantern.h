4114171790e62304b3da93081da7bad3d9a13c7d
Javier Luraschi <javierluraschi@hotmail.com>
2020-07-07 16:50:01 -0700

    define lantern host api override

--- lantern/include/lantern/lantern.h
+++ lantern/include/lantern/lantern.h
@@ -11,11 +11,13 @@
 #ifdef LANTERN_BUILD
 #define LANTERN_PTR
 #define LANTERN_HEADERS_ONLY
+#define HOST_API //
 #ifdef _WIN32
 #define LANTERN_API extern "C" __declspec(dllexport)
 #endif
 #else
 #define LANTERN_PTR *
+#define HOST_API
 #endif
 
 #ifndef LANTERN_API
