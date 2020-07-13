f25900eee1754676adc9ceaf19edc931a36ac5a6
Daniel Falbel <dfalbel@gmail.com>
2020-07-13 14:00:47 -0300

    use crossentropy loss

--- vignettes/examples/mnist-alexnet.R
+++ vignettes/examples/mnist-alexnet.R
@@ -5,7 +5,7 @@ ds <- tiny_imagenet_dataset(
   download = TRUE, 
   transform = function(x) {
     x <- magick::image_resize(x, "224x224")
-    x <- as.integer(magick::image_data(d[1][[1]], "rgb"))
+    x <- as.integer(magick::image_data(x, "rgb"))
     x <- torch_tensor(x)
     x <- x/256
     x <- x$permute(c(2, 0, 1))
@@ -49,7 +49,6 @@ net <- nn_module(
     x <- self$avgpool(x)
     x <- torch_flatten(x, start_dim = 2)
     x <- self$classifier(x)
-    nnf_log_softmax(x,dim = 1)
   }
 )
 
@@ -62,6 +61,7 @@ if (cuda_is_available()) {
 model <- net(num_classes = 200)
 model$to(device = device)
 optimizer <- optim_sgd(model$parameters, lr = 0.01)
+loss_fun <- nn_cross_entropy_loss()
 
 epochs <- 10
 
@@ -76,7 +76,7 @@ for (epoch in 1:10) {
   for (b in enumerate(dl)) {
     optimizer$zero_grad()
     output <- model(b[[1]]$to(device = device))
-    loss <- nnf_nll_loss(output, b[[2]]$to(device = device))
+    loss <- loss_fun(output, b[[2]]$to(device = device))
     loss$backward()
     optimizer$step()
     l <- c(l, loss$item())
