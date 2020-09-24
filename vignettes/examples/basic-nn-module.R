179eb288cc689943bffc715a1a1440a481b7549b
Daniel Falbel <dfalbel@gmail.com>
2020-09-24 00:18:44 -0300

    add a few examples

---
+++ vignettes/examples/basic-nn-module.R
@@ -0,0 +1,39 @@
+library(torch)
+
+# creates example tensors. x requires_grad = TRUE tells that 
+# we are going to take derivatives over it.
+dense <- nn_module(
+  clasname = "dense",
+  # the initialize function tuns whenever we instantiate the model
+  initialize = function(in_features, out_features) {
+    
+    # just for you to see when this function is called
+    cat("Calling initialize!") 
+    
+    # we use nn_parameter to indicate that those tensors are special
+    # and should be treated as parameters by `nn_module`.
+    self$w <- nn_parameter(torch_randn(in_features, out_features))
+    self$b <- nn_parameter(torch_zeros(out_features))
+    
+  },
+  # this function is called whenever we call our model on input.
+  forward = function(x) {
+    cat("Calling forward!")
+    torch_mm(x, self$w) + self$b
+  }
+)
+
+model <- dense(3, 1)
+
+# you can get all parameters 
+model$parameters
+
+# or individually
+model$w
+model$b
+
+# create an input tensor
+x <- torch_randn(10, 3)
+y_pred <- model(x)
+y_pred
+
