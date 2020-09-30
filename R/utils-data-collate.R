843b11a94ea812fe0b590dfa0edab4a6ff53cd3d
Daniel Falbel <dfalbel@gmail.com>
2020-09-30 18:44:23 -0300

    supporting R types in the default collate function

--- R/utils-data-collate.R
+++ R/utils-data-collate.R
@@ -1,11 +1,37 @@
 utils_data_default_collate <- function(batch) {
   elem <- batch[[1]]
-  if (is_torch_tensor(elem))
+  if (is_torch_tensor(elem)) {
+    
     return(torch_stack(batch, dim = 1))
-  else if (is.list(elem)) {
+    
+  } else if ((!is.character(elem) && !is.list(elem)) && is.atomic(elem) && 
+             (is.array(elem) || is.matrix(elem) || length(elem) > 1)) {
+    # we want arrays, matrix's and vectors (lenght > 1) to be converted to
+    # tensors. this is only different fromr torch because there's differentiation
+    # between lenght 1 vectors and scalars.
+    return(
+      utils_data_default_collate(
+        lapply(batch, function(x) torch_tensor(x))
+      )
+    )
+    
+  } else if (is.numeric(elem) && length(elem) == 1) {
+    
+    # we can safely unlist everything into a tensor
+    # we convert to integers because it's the most likely type.
+    k <- unlist(batch)
+    return(torch_tensor(k, dtype = torch_int64()))
+    
+  }  else if (is.character(elem) && length(elem) == 1) {
+    
+    return(unlist(batch))
+    
+  } else if (is.list(elem)) {
+    
     lapply(seq_along(elem), function(i) {
       utils_data_default_collate(lapply(batch, function(x) x[[i]]))
     })
+    
   } else 
     value_error("Can't collate data of class: '{class(data)}'")
 }
