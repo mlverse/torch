57d6c161e9478517480277b83deef5b73dee7d7c
Krzysztof Joachimiak <kjoa.92@gmail.com>
2020-10-17 00:12:56 +0200

    ASGD optimizer

---
+++ tests/testthat/test-optim-asgd.R
@@ -0,0 +1,6 @@
+test_that("optim_asgd", {
+  expect_optim_works(optim_asgd, list(lr = 0.1))
+  expect_optim_works(optim_asgd, list(lr = 0.1, alpha=0.7))
+  expect_optim_works(optim_asgd, list(lr = 0.1, alpha=0.7, t0=1e5))
+  expect_optim_works(optim_asgd, list(lr = 0.1, weight_decay = 1e-5))
+})
