a04f9fd4d5ec92587a5c009db0520879cdf94970
Daniel Falbel <dfalbel@gmail.com>
2020-10-26 15:18:38 -0300

    add test that state is correctly modified

--- tests/testthat/test-optim-adadelta.R
+++ tests/testthat/test-optim-adadelta.R
@@ -2,4 +2,5 @@ test_that("optim_adadelta", {
   expect_optim_works(optim_adadelta, list())
   expect_optim_works(optim_adadelta, list(rho = 0.9))
   expect_optim_works(optim_adadelta, list(rho = 0.9, weight_decay = 1e-5))
+  expect_state_is_updated(optim_adadelta)
 })
