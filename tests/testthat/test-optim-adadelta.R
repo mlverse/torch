05af0b78a781338ccb0c32ddf81c5b030e292da0
Daniel Falbel <dfalbel@gmail.com>
2020-11-03 21:24:10 -0300

    force seed

--- tests/testthat/test-optim-adadelta.R
+++ tests/testthat/test-optim-adadelta.R
@@ -1,4 +1,5 @@
 test_that("optim_adadelta", {
+  torch_manual_seed(1)
   expect_optim_works(optim_adadelta, list())
   expect_optim_works(optim_adadelta, list(rho = 0.9))
   expect_optim_works(optim_adadelta, list(rho = 0.9, weight_decay = 1e-5))
