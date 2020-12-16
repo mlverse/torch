d483bda8ba8ec20831b5ff7c365c0f2caae4940f
Daniel Falbel <dfalbel@gmail.com>
2020-12-16 10:38:13 -0300

    print test outputs

--- tools/check.sh
+++ tools/check.sh
@@ -5,3 +5,9 @@ Rscript -e "remotes::install_deps(dependencies = TRUE)"
 
 # check -----------
 Rscript -e 'rcmdcheck::rcmdcheck(args = c("--no-manual", "--no-multiarch"), error_on = "error", check_dir = "check")'
+
+# install ---------
+Rscript -e 'torch_package <- dir("check", full.names = TRUE, pattern = "torch_");install.packages(torch_package, repos = NULL, type = "source", INSTALL_opts = "--no-multiarch")'
+
+# run tests -------
+Rscript -e 'setwd("tests"); source("testthat.R")'
