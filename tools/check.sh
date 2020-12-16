26efeb57ba6b3b757d59cad0f533f229cd8548fb
Daniel Falbel <dfalbel@gmail.com>
2020-12-16 11:52:23 -0300

    add env vars

--- tools/check.sh
+++ tools/check.sh
@@ -1,15 +1,12 @@
 cd torch
 
+export TORCH_TEST=1
+export TORCH_INSTALL=1
+
 Rscript -e "install.packages(c('remotes', 'rcmdcheck'))"
 Rscript -e "remotes::install_deps(dependencies = TRUE)"
 
 # check -----------
 Rscript -e 'rcmdcheck::rcmdcheck(args = c("--no-manual", "--no-multiarch"), error_on = "error", check_dir = "check")'
-
-# install ---------
-Rscript -e 'torch_package <- dir("check", full.names = TRUE, pattern = "torch_");install.packages(torch_package, repos = NULL, type = "source", INSTALL_opts = "--no-multiarch")'
-
-# run tests -------
-Rscript -e 'setwd("tests"); source("testthat.R")'
 
 echo "Done"
