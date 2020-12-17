e6f53b1a785f7af18ef9a4850351e67388db2193
Daniel Falbel <dfalbel@gmail.com>
2020-12-17 08:49:27 -0300

    make shell fail if the check fails

--- tools/check.sh
+++ tools/check.sh
@@ -7,6 +7,7 @@ Rscript -e "install.packages(c('remotes', 'rcmdcheck'))"
 Rscript -e "remotes::install_deps(dependencies = TRUE)"
 
 # check -----------
-Rscript -e 'rcmdcheck::rcmdcheck(args = c("--no-manual", "--no-multiarch"), error_on = "error", check_dir = "check")'
+Rscript -e 'check <- rcmdcheck::rcmdcheck(args = c("--no-manual", "--no-multiarch"), error_on = "never")' \
+        -e 'quit(status = check$status)'
 
-echo "Done"
+exit $?
