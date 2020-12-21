f6c3454afad3fdac826e6209f121a4358c48361b
Daniel Falbel <dfalbel@gmail.com>
2020-12-21 10:54:20 -0300

    build lantern in GPU builds

--- tools/check.sh
+++ tools/check.sh
@@ -6,6 +6,11 @@ export TORCH_INSTALL=1
 Rscript -e "install.packages(c('remotes', 'rcmdcheck'))"
 Rscript -e "remotes::install_deps(dependencies = TRUE)"
 
+if [$BUILD_LANTERN == "true"]
+then
+  Rscript tools/buildlantern.R
+fi
+
 # check -----------
 Rscript -e 'check <- rcmdcheck::rcmdcheck(args = c("--no-manual", "--no-multiarch"), error_on = "never")' \
         -e 'quit(status = check$status)'
