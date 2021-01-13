d15f9848766bf3d524c022f0a2d8a770e3ba2714
Daniel Falbel <dfalbel@gmail.com>
2021-01-13 13:02:38 -0300

    add layer norm

---
+++ tests/testthat/test-nn-normalization.R
@@ -0,0 +1,26 @@
+test_that("layer_norm", {
+
+  input <- torch_tensor(t(matrix(1:3, ncol = 3, nrow = 3)), dtype = torch_float())
+  
+  m <- nn_layer_norm(3, elementwise_affine = TRUE)
+  result <- matrix(
+    c(-1.22473537921906, -1.22473537921906, -1.22473537921906, 
+      0, 0, 0, 1.22473549842834, 1.22473549842834, 1.22473549842834), 
+    nrow = 3, ncol = 3
+  )
+  expect_equal_to_r(
+    m(input),
+    result
+  )
+  
+  m <- nn_layer_norm(3, elementwise_affine = FALSE)
+  expect_equal_to_r(
+    m(input),
+    result
+  )
+  
+  input <- torch_randn(3,4,5)
+  m <- nn_layer_norm(input$size()[-1])
+  expect_tensor_shape(m(input), c(3,4,5))
+  
+})
