ff51b7d6c7d2119150359180fb2d3cb290bfa5b1
Daniel Falbel <dfalbel@gmail.com>
2021-01-24 10:52:29 -0300

    more reliably set the external pointer address

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -29262,17 +29262,6 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
-// xptr_address
-SEXP xptr_address(SEXP s);
-RcppExport SEXP _torch_xptr_address(SEXP sSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< SEXP >::type s(sSEXP);
-    rcpp_result_gen = Rcpp::wrap(xptr_address(s));
-    return rcpp_result_gen;
-END_RCPP
-}
 // set_xptr_address
 SEXP set_xptr_address(SEXP s, SEXP p);
 RcppExport SEXP _torch_set_xptr_address(SEXP sSEXP, SEXP pSEXP) {
@@ -31532,7 +31521,6 @@ static const R_CallMethodDef CallEntries[] = {
     {"_torch_cpp_make_function_name", (DL_FUNC) &_torch_cpp_make_function_name, 5},
     {"_torch_cpp_torch_variable_list", (DL_FUNC) &_torch_cpp_torch_variable_list, 1},
     {"_torch_cpp_variable_list_to_r_list", (DL_FUNC) &_torch_cpp_variable_list_to_r_list, 1},
-    {"_torch_xptr_address", (DL_FUNC) &_torch_xptr_address, 1},
     {"_torch_set_xptr_address", (DL_FUNC) &_torch_set_xptr_address, 2},
     {"_torch_set_xptr_protected", (DL_FUNC) &_torch_set_xptr_protected, 2},
     {NULL, NULL, 0}
