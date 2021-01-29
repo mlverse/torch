ffdc8701690961e668f6db8f106e4ae2197be917
Daniel Falbel <dfalbel@gmail.com>
2021-01-29 12:12:54 -0300

    add unit texst

--- tests/testthat/test-allocator.R
+++ tests/testthat/test-allocator.R
@@ -8,3 +8,17 @@ test_that("allocator calls gc", {
   expect_error(torch_randn(1e9, 1e9))
   expect_true(env$gc_called)
 })
+
+test_that("gpu allocator is called", {
+  
+  skip_if_cuda_not_available()
+  
+  env <- new.env()
+  trace(quote(gc), print = FALSE, function() {
+    env$gc_called <- TRUE
+  })
+  
+  expect_error({torch_randn(1e6, 1e7, device = "cuda")})
+  expect_true(env$gc_called)
+  
+})
