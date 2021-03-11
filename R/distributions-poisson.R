15dbe166fa7b681bcb4018bbb28eef5da4dbbf50
Krzysztof Joachimiak <kjoa.92@gmail.com>
2021-03-11 23:44:59 +0100

    Poisson distribution

---
+++ R/distributions-poisson.R
@@ -0,0 +1,80 @@
+#' @include distributions.R
+#' @include distributions-exp-family.R
+#' @include distributions-utils.R
+#' @include distributions-constraints.R
+#' @include utils.R
+
+Poisson <- R6::R6Class(
+  "torch_Poisson",
+  lock_objects = FALSE,
+  inherit = ExponentialFamily,
+  
+  public = list(
+    initialize = function(rate, validate_args = NULL){
+      self$rate <- broadcast_all(list(rate))[[1]]
+      if (inherits(rate, "numeric"))
+        batch_shape <- 1
+      else
+        batch_shape <- self$rate$size()
+      super$initialize(batch_shape, validate_args=validate_args)
+    },
+    
+    expand = function(batch_shape, .instance){
+      new <- self$.get_checked_instance(self, .instance)
+      new$rate <- self$rate$expand(batch_shape)
+      new$.__enclos_env__$super$initialize(batch_shape, validate_args = FALSE)
+      new$.validate_args <- self$.validate_args
+      new
+    },
+    
+    sample = function(sample_shape = NULL){
+      shape <- self$.extended_shape(sample_shape)
+      with_no_grad({
+        torch_poisson(self$rate$expand(shape))
+      })
+    },
+      
+    log_prob = function(value){
+      if (self$.validate_args)
+        self$.validate_sample(value)
+      results <- broadcast_all(list(self$rate, value))
+      rate  <- results[[1]]
+      value <- results[[2]]
+      (rate$log() * value) - rate - (value + 1)$lgamma()
+    }
+  ),
+  
+  private = list(
+    .log_normalizer = function(x){
+      torch_exp(x)
+    }
+  ),
+  
+  active = list(
+    mean = function(){
+      self$rate
+    },
+    
+    variance = function(){
+      self$rate
+    },
+    
+    .natural_params = function(){
+      torch_log(self$rate)
+    }
+  )
+)
+
+#' Creates a Poisson distribution parameterized by `rate`, the rate parameter.
+#' Samples are nonnegative integers, with a pmf given by
+#' \eqn{\mathrm{rate}^k \frac{e^{-\mathrm{rate}}}{k!}}
+#' 
+#' @param rate (numeric, torch_tensor): the rate parameter
+#' 
+#' @examples 
+#' m <- distr_poisson(torch_tensor(4))
+#' m$sample()
+#' @export
+distr_poisson <- function(rate, validate_args = NULL){
+  Poisson$new(rate, validate_args)
+}
---
+++ tests/testthat/test-distribution-poisson.R
@@ -0,0 +1,27 @@
+#' Note: consider PyTorch - like test schema
+#' See: https://github.com/pytorch/pytorch/blob/master/test/distributions/test_distributions.py
+#' TODO: add more unit tests
+
+test_that("Poisson distribution - shape test", {
+  rate    <- torch_randn(2, 3)$abs()$requires_grad_()
+  rate_1d <- torch_randn(1)$abs()$requires_grad_()
+  expect_equal(distr_poisson(rate)$sample()$size(), c(2, 3))
+  expect_equal(distr_poisson(rate_1d)$sample()$size(), 1)
+  expect_equal(distr_poisson(rate_1d)$sample(1)$size(), c(1, 1))
+  expect_equal(distr_poisson(2.0)$sample(2)$size(), c(2, 1))
+})
+
+test_that("Poisson distribution - log_prob", {
+  rate    <- torch_randn(2, 3)$abs()$requires_grad_()
+  rate_1d <- torch_randn(1)$abs()$requires_grad_()
+  
+  ref_log_prob <- function(idx, x, log_prob){
+    l <- rate$view(-1)[idx]$detach()
+    l <- as.vector(as.array(l))
+    x <- as.vector(as.array(x))
+    expected <- ppois(x, l, log.p = TRUE)
+    expect_equal(log_prob, torch_tensor(expected))
+  }
+  
+  check_log_prob(distr_poisson(rate), ref_log_prob)
+})
