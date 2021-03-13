fe4140ee58bc9898e4652684aa9e6585325c05f6
kevinykuo <kevinykuo@gmail.com>
2021-03-12 16:19:39 -0800

    $ instead of .

--- R/nn-activation.R
+++ R/nn-activation.R
@@ -815,7 +815,7 @@ nn_multihead_attention <- nn_module(
         query, key, value, self$embed_dim, self$num_heads,
         self$in_proj_weight, self$in_proj_bias,
         self$bias_k, self$bias_v, self$add_zero_attn,
-        self$dropout, self$out_proj.weight, self$out_proj.bias,
+        self$dropout, self$out_proj$weight, self$out_proj$bias,
         training=self$training,
         key_padding_mask=key_padding_mask, need_weights=need_weights,
         attn_mask=attn_mask, use_separate_proj_weight=TRUE,
@@ -826,7 +826,7 @@ nn_multihead_attention <- nn_module(
         query, key, value, self$embed_dim, self$num_heads,
         self$in_proj_weight, self$in_proj_bias,
         self$bias_k, self$bias_v, self$add_zero_attn,
-        self$dropout, self$out_proj.weight, self$out_proj.bias,
+        self$dropout, self$out_proj$weight, self$out_proj$bias,
         training=self$training,
         key_padding_mask=key_padding_mask, need_weights=need_weights,
         attn_mask=attn_mask)
