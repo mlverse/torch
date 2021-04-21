0f472aec88617e6d5ef981dadbd8057bd1da2c9a
Daniel Falbel <dfalbel@gmail.com>
2021-04-21 12:46:19 -0300

    Add optimizer function allowing to create custom optimizers.

--- R/optim.R
+++ R/optim.R
@@ -126,3 +126,74 @@ state <- function(self) {
   attr(self, "state") <- value
   self
 }
+
+#' Creates a custom optimizer
+#' 
+#' When implementing custom optimizers you will usually need to implement
+#' the `initialize` and `step` methods. See the example section below
+#' for a full example.
+#' 
+#' @param name (optional) name of the optimizer
+#' @param inherit (optional) you can inherit from other optimizers to re-use
+#'   some methods.
+#' @param ... Pass any number of fields or methods. You should at least define
+#'   the `initialize` and `step` methods. See the examples section.
+#' @param private (optional) a list of private methods for the optimizer.
+#' @param active (optional) a list of active methods for the optimizer.
+#' @param parent_env used to capture the right environment to define the class.
+#'   The default is fine for most situations.
+#' 
+#' @examples 
+#' 
+#' # In this example we will create a custom optimizer
+#' # that's just a simplified version of the `optim_sgd` function.
+#' 
+#' optim_sgd2 <- optimizer(
+#'   initialize = function(params, learning_rate) {
+#'     defaults <- list(
+#'       learning_rate = learning_rate
+#'     )
+#'     super$initialize(params, defaults)
+#'   },
+#'   step = function() {
+#'     with_no_grad({
+#'       for (g in seq_along(self$param_groups)) {
+#'         group <- self$param_groups[[g]]
+#'         for (p in seq_along(group$params)) {
+#'           param <- group$params[[p]]
+#'           
+#'           if (is.null(param$grad) || is_undefined_tensor(param$grad))
+#'             next
+#'           
+#'           param$add_(param$grad, alpha = -group$learning_rate)
+#'         }
+#'       }
+#'     })
+#'   }
+#' )
+#' 
+#' x <- torch_randn(1, requires_grad = TRUE)
+#' opt <- optim_sgd2(x, learning_rate = 0.1)
+#' for (i in 1:100) {
+#'   opt$zero_grad()
+#'   y <- x^2
+#'   y$backward()
+#'   opt$step()
+#' }
+#' all.equal(x$item(), 0, tolerance = 1e-9)
+#' 
+#'
+#' @export
+optimizer <- function(name = NULL, inherit = Optimizer, ..., 
+                    private = NULL, active = NULL,
+                    parent_env = parent.frame()) {
+  create_class(
+    name = name, 
+    inherit = inherit,
+    ...,
+    private = private, 
+    active = active,
+    parent_env = parent_env,
+    attr_name = "Optimizer"
+  )
+}
