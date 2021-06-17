2f4e872208f571a75d95740fbd6d3cbd948a88be
Daniel Falbel <dfalbel@gmail.com>
2021-06-17 16:13:42 -0300

    make sure we can modify the paramters in place

--- tests/testthat/test-script_module.R
+++ tests/testthat/test-script_module.R
@@ -6,4 +6,17 @@ test_that("script module parameters", {
   expect_equal(names(parameters), c("weight", "bias"))
   expect_tensor_shape(parameters$weight, c(10, 10))
   expect_tensor_shape(parameters$bias, c(10))
+  
+})
+
+test_that("parameters are modifiable in-place", {
+  script_module <- jit_load("assets/linear.pt")
+  parameters <- script_module$parameters
+  
+  with_no_grad({
+    parameters$weight$zero_()  
+  })
+  
+  parameters <- script_module$parameters
+  expect_equal_to_tensor(parameters$weight, torch_zeros(10, 10))
 })
