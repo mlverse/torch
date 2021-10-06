8cdb00399beebb9d44b52784d1972b7bfa15a835
Daniel Falbel <dfalbel@gmail.com>
2021-10-06 16:38:14 -0300

    add revdepcheck script to tools.

---
+++ tools/revdep.R
@@ -0,0 +1,6 @@
+
+revdepcheck::revdep_check(num_workers = 4, pkg = ".", 
+                          env = c(revdepcheck::revdep_env_vars(),
+                                  TORCH_INSTALL = "1", 
+                                  TORCH_TEST = "1"))
+
