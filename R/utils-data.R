f15f65b53bc3aa5ff064ee626845a49ee33cc315
Daniel Falbel <dfalbel@gmail.com>
2022-01-07 10:09:42 -0300

    Rebuild docs.

--- R/utils-data.R
+++ R/utils-data.R
@@ -24,17 +24,18 @@ get_init <- function(x) {
 
 #' Helper function to create an function that generates R6 instances of class `dataset`
 #' 
-#' The output is a function `f` with class `dataset_generator`. Calling `f()`
-#' creates a new instance of the R6 class `dataset`. The R6 class is stored in the 
-#' enclosing environment of `f` and can also be accessed through `f`s attribute 
-#' `Dataset`.
-
 #' All datasets that represent a map from keys to data samples should subclass this 
 #' class. All subclasses should overwrite the `.getitem()` method, which supports 
 #' fetching a data sample for a given key. Subclasses could also optionally 
 #' overwrite `.length()`, which is expected to return the size of the dataset 
 #' (e.g. number of samples) used by many sampler implementations 
 #' and the default options of [dataloader()].
+#' 
+#' @returns 
+#' The output is a function `f` with class `dataset_generator`. Calling `f()`
+#' creates a new instance of the R6 class `dataset`. The R6 class is stored in the 
+#' enclosing environment of `f` and can also be accessed through `f`s attribute 
+#' `Dataset`.
 #' 
 #' @section Get a batch of observations:
 #' 
