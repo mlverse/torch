49d6f2b55802401eb59cd4c93accd2aee6306192
Hamada S. Badr <hamada.s.badr@gmail.com>
2022-01-13 21:16:03 -0500

    `tools/style.sh`: Exclude `check` directories

--- tools/style.sh
+++ tools/style.sh
@@ -12,8 +12,8 @@ Rscript -e 'styler::style_pkg(exclude_files = list.files("./R", pattern = "^gen-
 
 
 # Style/format C/C++ code
-find . -type f \( -name '*.h' -o -name '*.hpp' -o -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.cxx' \) ! -path "*/gen-*.*" ! -path "*/lantern.*"  ! -path "*/RcppExports.*" -exec clang-format -style=Google --verbose -i {} \;
+find -not -path "./check/*" . -type f \( -name '*.h' -o -name '*.hpp' -o -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.cxx' \) ! -path "*/gen-*.*" ! -path "*/lantern.*"  ! -path "*/RcppExports.*" -exec clang-format -style=Google --verbose -i {} \;
 
 # Remove whitespaces
-find . -type f \( -name 'DESCRIPTION' -o -name "*.R" \) ! -path "*/gen-*.*" ! -path "*/RcppExports.*" -exec perl -pi -e 's/[ \t]*$//' {} \;
-find . -type f \( -name '*.h' -o -name '*.hpp' -o -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.cxx' \) ! -path "*/gen-*.*" ! -path "*/lantern.*"  ! -path "*/RcppExports.*" -exec perl -pi -e 's/[ \t]*$//' {} \;
+find -not -path "./check/*" . -type f \( -name 'DESCRIPTION' -o -name "*.R" \) ! -path "*/gen-*.*" ! -path "*/RcppExports.*" -exec perl -pi -e 's/[ \t]*$//' {} \;
+find -not -path "./check/*" . -type f \( -name '*.h' -o -name '*.hpp' -o -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.cxx' \) ! -path "*/gen-*.*" ! -path "*/lantern.*"  ! -path "*/RcppExports.*" -exec perl -pi -e 's/[ \t]*$//' {} \;
