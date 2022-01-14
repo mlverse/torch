57374668e572bafdbb4cc0421861dfb9a6f59250
Hamada S. Badr <hamada.s.badr@gmail.com>
2022-01-14 00:48:36 -0500

    `tools/style.sh`: Generate diffstat

--- tools/style.sh
+++ tools/style.sh
@@ -13,10 +13,12 @@ Rscript -e 'styler::style_pkg(exclude_files = list.files("./R", pattern = "^gen-
 
 # Style/format C/C++ code
 find . -type f \( -name '*.h' -o -name '*.hpp' -o -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.cxx' \) ! -path "*/gen-*.*" ! -path "*/lantern.*"  ! -path "*/RcppExports.*" ! -path "./check/*" -printf '%p\n' -exec clang-format -style=Google --verbose -i {} \;
+git diff --stat
 
 # Remove whitespaces
 find . -type f \( -name 'DESCRIPTION' -o -name "*.R" \) ! -path "*/gen-*.*" ! -path "*/RcppExports.*" ! -path "./check/*" -printf '%p\n' -exec perl -pi -e 's/[ \t]*$//' {} \;
 find . -type f \( -name '*.h' -o -name '*.hpp' -o -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.cxx' \) ! -path "*/gen-*.*" ! -path "*/lantern.*"  ! -path "*/RcppExports.*" ! -path "./check/*" -printf '%p\n' -exec perl -pi -e 's/[ \t]*$//' {} \;
+git diff --stat
 
 # Render documents
 Rscript -e "if (!require('rmarkdown')) install.packages('rmarkdown')"
@@ -25,3 +27,4 @@ if [ -f README.Rmd ]; then
   Rscript -e 'rmarkdown::render("README.Rmd", output_format = "md_document")'
 fi
 Rscript -e 'roxygen2::roxygenize()'
+git diff --stat
