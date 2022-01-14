9a41ec215a5d31959977ae1475893463ceb32779
Daniel Falbel <dfalbel@gmail.com>
2022-01-14 13:47:08 -0300

    re-generare with fixed seed + ignore whitespace changes in untracked build directories.

--- tools/style.sh
+++ tools/style.sh
@@ -23,7 +23,16 @@ git diff --stat
 
 # Remove whitespaces
 find . -type f \( -name 'DESCRIPTION' -o -name "*.R" \) ! -path "*/gen-*.*" ! -path "*/RcppExports.*" ! -path "./check/*" -printf '%p\n' -exec perl -pi -e 's/[ \t]*$//' {} \;
-find . -type f \( -name '*.h' -o -name '*.hpp' -o -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.cxx' \) ! -path "*/gen-*.*" ! -path "*/lantern.*"  ! -path "*/RcppExports.*" ! -path "./check/*" -printf '%p\n' -exec perl -pi -e 's/[ \t]*$//' {} \;
+find . -type f \( -name '*.h' -o -name '*.hpp' -o -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.cxx' \) \
+  ! -path "*/gen-*.*" ! -path "*/lantern.*"  ! -path "*/RcppExports.*" ! -path "./check/*" \
+  -not -path './lantern/build/*' \
+  -not -path './lantern/headers/build/*' \
+  -not -path "./inst/include/*/*" \
+  -not -path "./revdep/*" \ 
+  ! -path "./check/*" \
+  -printf '%p\n' \
+  -exec perl -pi -e 's/[ \t]*$//' {} \;
+
 git diff --stat
 
 # Render documents
@@ -31,6 +40,6 @@ Rscript -e "if (!require('rmarkdown')) install.packages('rmarkdown')"
 Rscript -e "if (!require('roxygen2')) install.packages('roxygen2')"
 Rscript tools/document.R
 if [ -f README.Rmd ]; then
-  Rscript -e 'rmarkdown::render("README.Rmd", output_format = "md_document")'
+  Rscript -e 'rmarkdown::render("README.Rmd", output = "github_document")'
 fi
 git diff --stat
