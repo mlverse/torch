eb632eccfa50a5bad8b0d78ec8ebe6de39ca211e
Daniel Falbel <dfalbel@gmail.com>
2022-05-13 13:46:16 -0300

    Add manual tests that compare with PyTorch results.

--- tests/testthat/test-nn-sparse.R
+++ tests/testthat/test-nn-sparse.R
@@ -34,4 +34,38 @@ test_that("nn_embedding_bag", {
   
   expect_equal_to_tensor(output[1, ], embedding$weight[3, ] + embedding$weight[6, ])
   expect_equal_to_tensor(output[2, ], embedding$weight[2, ] + embedding$weight[4, ] + embedding$weight[5, ])
+  
+  # a fixed seed should give the same weights as in python
+  torch::torch_manual_seed(1)
+  embedding <- nn_embedding_bag(10, 1)
+  
+  # this vector was obtained by running 
+  # torch.manual_seed(1)
+  # embedding = torch.nn.EmbeddingBag(10, 1)
+  weights_py <-  c(0.6614,
+                 0.2669,
+                 0.0617,
+                 0.6213,
+                -0.4519,
+                -0.1661,
+                -1.5228,
+                 0.3817,
+                -1.0276,
+                -0.5631)
+  
+  expect_equal_to_r(
+    embedding$weight,
+    matrix(weights_py, ncol = 1),
+    tolerance = 1e-4
+  )
+  
+  # input = torch.tensor([[1,2,4,5], [4,3,2,9]]) - 1
+  # embedding(input)
+  py_out <- c(0.2744, -0.0194)
+  expect_equal_to_r(
+    embedding(input),
+    matrix(py_out, ncol = 1),
+    tolerance = 1e-4
+  )
+  
 })
