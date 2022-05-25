9f9bdc5c967973f015cfb8ec62856b75dc2b9669
Daniel Falbel <dfalbel@gmail.com>
2022-05-25 15:29:18 -0300

    Add other small checks.

--- tests/testthat/test-nn-activation.R
+++ tests/testthat/test-nn-activation.R
@@ -57,10 +57,17 @@ test_that("Multihead attention works", {
   expect_equal_to_tensor(res1[[1]], res2[[1]]$transpose(2,1))
   expect_equal_to_tensor(res1[[2]], res2[[2]])
   
+  # comparing to python results.
   torch::torch_manual_seed(1)
   attn1 <- nn_multihead_attention(embed_dim = 2, num_heads = 1)
+  x <- torch_randn(1,1,2)
+  out <- attn1(x, x, x)
+  
+  expect_equal_to_r(out[[1]][1,1,], c(0.0736, -0.0599), tol = 1e-4)
+  expect_equal_to_r(out[[2]][1,1,], c(1), tol = 1e-4)
   expect_equal_to_r(attn1$in_proj_weight[1,], c(-0.1782,  0.4406), tol = 1e-4)
   expect_equal_to_r(attn1$out_proj$weight[1,], c(0.3643, -0.3121), tol = 1e-4)
   
+  # raise error when embed_dim is not divisible by num_heads.
   expect_error(nn_multihead_attention(embed_dim = 512, num_heads = 10), regexp="divisible")
 })
