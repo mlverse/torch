e61a42537d5f331a927ffc3f29c8c0ac6c9501ef
Daniel Falbel <dfalbel@gmail.com>
2022-06-02 14:56:19 -0300

    try different way of finding the function.

--- src/translate_messages.h
+++ src/translate_messages.h
@@ -1,6 +1,5 @@
 // translate error messages
 std::string translate_error_message(std::string msg) {
-  auto env = Rcpp::Environment::namespace_env("torch");
-  Rcpp::Function f = env["translate_error_msg"];
+  Rcpp::Function f = Rcpp::Environment::namespace_env("torch").find("translate_error_msg");
   return Rcpp::as<std::string>(f(msg));
 }
