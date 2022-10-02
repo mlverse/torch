9613dc2e792a7ea6008309514af41c804b5e392f
Daniel Falbel <dfalbel@gmail.com>
2022-10-02 15:45:58 -0300

    Handle `SymInt`
    Also add test cases for function that use some of those.

--- tests/testthat/test-tensor.R
+++ tests/testthat/test-tensor.R
@@ -469,3 +469,20 @@ test_that("create complex from and to R", {
   expect_equal(as.complex(x), complex(real = 1,imag = 1))
   
 })
+
+test_that("expand works", {
+  x <- torch_tensor(1)
+  y <- x$expand(c(1, 2, 3))
+  expect_equal(y$size(), c(1, 2, 3))
+  y <- x$expand(c(2, 2, 3), TRUE)
+  expect_equal(y$size(), c(2, 2, 3))
+  y <- x$expand(c(2, 2, 3), FALSE)
+  expect_equal(y$size(), c(2, 2, 3))
+})
+
+test_that("narrow_copy works", {
+  x <- torch_tensor(1:10)
+  y <- torch_narrow_copy(x, dim = 1, start = 1, length = 3)
+  expect_equal(y$size(), c(3))
+  expect_equal(y, torch_tensor(2:4))
+})
