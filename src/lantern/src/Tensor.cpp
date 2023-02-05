1d0672f5f53a917d874341462b33db573ad18bf5
Daniel Falbel <dfalbel@gmail.com>
2023-02-05 20:53:54 +0100

    Use the new interpreter def based on the vtable.

--- src/lantern/src/Tensor.cpp
+++ src/lantern/src/Tensor.cpp
@@ -275,20 +275,100 @@ void *_lantern_normal_tensor_double_generator(void *mean, double std,
   LANTERN_FUNCTION_END
 }
 
-std::string lantern_name_sig(const c10::impl::PyInterpreter *x) {
-  return "LanternInterpreter";
-}
-
-bool lantern_is_contiguous_sig (const c10::impl::PyInterpreter*, const at::TensorImpl*) {
-  return true;
-}
-
 // global interpreter definition.
 // unlike in python, we currently don't have behaviors where a tensor
 // can be owned by different interpreters.
 
-c10::impl::PyInterpreter lantern_interpreter(*lantern_name_sig, nullptr,
-                                             nullptr, nullptr, *lantern_is_contiguous_sig);
+namespace c10 {
+namespace impl {
+
+struct NoopPyInterpreterVTable final : public PyInterpreterVTable {
+  std::string name() const override {
+    return "<unloaded interpreter>";
+  }
+
+  void decref(PyObject* pyobj, bool is_tensor) const override {} // do nothing
+
+#define PANIC(m)              \
+  TORCH_INTERNAL_ASSERT(      \
+      0,                      \
+      "attempted to call " #m \
+      " on a Tensor with nontrivial PyObject after corresponding interpreter died")
+
+  c10::intrusive_ptr<TensorImpl> detach(const TensorImpl* self) const override {
+    PANIC(detach);
+  }
+
+  void dispatch(const c10::OperatorHandle& op, torch::jit::Stack* stack)
+      const override {
+    PANIC(dispatch);
+  }
+
+  void python_dispatcher(
+      const c10::OperatorHandle& op,
+      c10::DispatchKeySet,
+      torch::jit::Stack* stack) const override {
+    PANIC(python_dispatcher);
+  }
+
+  bool is_contiguous(const TensorImpl* self, at::MemoryFormat) const override {
+    PANIC(is_contiguous);
+  }
+  bool is_strides_like(const TensorImpl* self, at::MemoryFormat)
+      const override {
+    PANIC(is_strides_like);
+  }
+  bool is_non_overlapping_and_dense(const TensorImpl* self) const override {
+    PANIC(is_non_overlapping_and_dense);
+  }
+  c10::Device device(const TensorImpl* self) const override {
+    PANIC(device);
+  }
+  int64_t dim(const TensorImpl* self) const override {
+    PANIC(dim);
+  }
+  c10::IntArrayRef strides(const TensorImpl* self) const override {
+    PANIC(strides);
+  }
+  c10::IntArrayRef sizes(const TensorImpl* self) const override {
+    PANIC(sizes);
+  }
+  c10::SymIntArrayRef sym_sizes(const TensorImpl* self) const override {
+    PANIC(sym_sizes);
+  }
+  c10::Layout layout(const TensorImpl* self) const override {
+    PANIC(layout);
+  }
+  c10::SymInt sym_numel(const TensorImpl* self) const override {
+    PANIC(sym_numel);
+  }
+  c10::SymIntArrayRef sym_strides(const TensorImpl* self) const override {
+    PANIC(sym_strides);
+  }
+  c10::SymInt sym_storage_offset(const TensorImpl* self) const override {
+    PANIC(sym_storage_offset);
+  }
+
+  // Just swallow the event, don't do anything
+  void trace_gpu_event_creation(uintptr_t event) const override {}
+  void trace_gpu_event_deletion(uintptr_t event) const override {}
+  void trace_gpu_event_record(uintptr_t event, uintptr_t stream)
+      const override {}
+  void trace_gpu_event_wait(uintptr_t event, uintptr_t stream) const override {}
+  void trace_gpu_memory_allocation(uintptr_t ptr) const override {}
+  void trace_gpu_memory_deallocation(uintptr_t ptr) const override {}
+  void trace_gpu_stream_creation(uintptr_t stream) const override {}
+  void trace_gpu_device_synchronization() const override {}
+  void trace_gpu_stream_synchronization(uintptr_t stream) const override {}
+  void trace_gpu_event_synchronization(uintptr_t event) const override {}
+
+};
+
+} // namespace impl
+} // namespace c10
+
+c10::impl::NoopPyInterpreterVTable* lantern_vtable;
+c10::impl::PyInterpreter lantern_interpreter(lantern_vtable);
 
 void _lantern_tensor_set_pyobj(void *x, void *ptr) {
   LANTERN_FUNCTION_START
