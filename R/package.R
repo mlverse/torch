9da4c40cc745d44dc5ee82d83d5c5c6db1ca190e
Daniel Falbel <dfalbel@gmail.com>
2023-04-04 09:49:42 +0200

    also special case shinyapps

--- R/package.R
+++ R/package.R
@@ -27,7 +27,7 @@ globalVariables(c("..", "self", "private", "N"))
   autoinstall <- autoinstall || (Sys.getenv("TORCH_INSTALL", unset = 2) == "1")
   
   # We can auto install in Posit Connect - if TORCH_INSTALL is not set to zero
-  is_connect <- Sys.getenv("R_CONFIG_ACTIVE") == "rsconnect"
+  is_connect <- Sys.getenv("R_CONFIG_ACTIVE") %in% c("rsconnect", "shinyapps")
   not_install <- Sys.getenv("TORCH_INSTALL", unset = "2") == "0"
   autoinstall <- autoinstall || (is_connect && !not_install)
   
