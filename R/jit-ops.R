f9694162a7ef863b4b141b3143429bd1de2ecc2c
Sigrid Keydana <469371+skeydan@users.noreply.github.com>
2023-05-03 11:31:33 +0200

    document jit_ops

--- R/jit-ops.R
+++ R/jit-ops.R
@@ -1,3 +1,21 @@
+#' Enable idiomatic access to JIT operators from R.
+#'
+#' Call JIT operators directly from R, keeping the familiar argument types and argument order.
+#' Note, however, that:
+#' - all arguments are required (no defaults)
+#' - axis numbering (as well as position numbers overall) starts from 0
+#' - scalars have to be wrapped in `jit_scalar()`
+#' 
+#' @examples
+#' t1 <- torch::torch_rand(4, 5)
+#' t2 <- torch::torch_ones(5, 4)
+#' # same as torch::torch_matmul(t1, t2)
+#' jit_ops$aten$matmul(t1, t2)
+#' 
+#' # same as torch_split(torch::torch_arange(0, 3), 2, 1)
+#' jit_ops$aten$split(torch::torch_arange(0, 3), torch::jit_scalar(2L), torch::jit_scalar(0L))
+#' 
+
 #' @export
 jit_ops <- structure(list(), class = "torch_ops")
 
--- tests/testthat/test-jit-ops.R
+++ tests/testthat/test-jit-ops.R
@@ -12,21 +12,21 @@ test_that("can access operators via ops object", {
   
   # split, returning two tensors in a list of length 2
   res_torch <- torch_split(torch::torch_arange(0, 3), 2, 1)
-  res_jit <- jit_ops$aten$split(torch::torch_arange(0, 3), jit_scalar(2L), jit_scalar(0L))
+  res_jit <- jit_ops$aten$split(torch::torch_arange(0, 3), torch::jit_scalar(2L), torch::jit_scalar(0L))
   expect_length(res_jit, 2)
   expect_equal_to_tensor(res_jit[[1]], res_torch[[1]])
   expect_equal_to_tensor(res_jit[[2]], res_torch[[2]])
   
   # split, returning a single tensor
   res_torch <- torch_split(torch::torch_arange(0, 3), 4, 1)
-  res_jit <- jit_ops$aten$split(torch::torch_arange(0, 3), jit_scalar(4L), jit_scalar(0L))
+  res_jit <- jit_ops$aten$split(torch::torch_arange(0, 3), torch::jit_scalar(4L), torch::jit_scalar(0L))
   expect_length(res_jit, 1)
   expect_equal_to_tensor(res_jit[[1]], res_torch[[1]])
   
   # linalg_qr always returns a list
   m <- torch_eye(5)/5
   res_torch <- linalg_qr(m)
-  res_jit <- jit_ops$aten$linalg_qr(m, jit_scalar("reduced"))
+  res_jit <- jit_ops$aten$linalg_qr(m, torch::jit_scalar("reduced"))
   expect_equal_to_tensor(res_torch[[2]], res_jit[[2]])
 })
 
