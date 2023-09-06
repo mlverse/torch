b0e0e8aac92371484ae4186b33f4f85e5dd1c743
Nacho Díez <119790922+nachodieez@users.noreply.github.com>
2023-09-06 22:06:57 +0200

    Solved `out_features` typo in `nn_linear`. (#1097)
    The public member of `nn_linear` for the output features should be called `out_features` instead of `out_feature` (s was missing).
    Solved the typo and updated `test-nn.R` to not fail with the new name.

--- R/nn-linear.R
+++ R/nn-linear.R
@@ -57,8 +57,8 @@ nn_identity <- nn_module(
 nn_linear <- nn_module(
   "nn_linear",
   initialize = function(in_features, out_features, bias = TRUE) {
-    self$in_features <- in_features
-    self$out_feature <- out_features
+    self$in_features  <- in_features
+    self$out_features <- out_features
 
     self$weight <- nn_parameter(torch_empty(out_features, in_features))
     if (bias) {
