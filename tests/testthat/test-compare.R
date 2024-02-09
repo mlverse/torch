ab3c1201eb84a654073c0e0a6f7b570e91192bd3
Sebastian Fischer <sebf.fischer@gmail.com>
2024-02-09 17:00:01 +0100

    test: add cuda test for compare_proxy

--- tests/testthat/test-compare.R
+++ tests/testthat/test-compare.R
@@ -16,4 +16,11 @@ test_that("can compare tensors with testthat edition 3", {
     torch_tensor(1),
     torch_tensor(2)
   ))
+
+  skip_if_no_cuda()
+
+  expect_failure(expect_equal(
+    torch_tensor(1)$cuda(),
+    torch_tensor(1)$cpu()
+  ))
 })
