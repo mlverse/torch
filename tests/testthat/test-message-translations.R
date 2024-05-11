1afa53fc5d90dfe28954dd354aee38514e59026d
C. Regouby <christophe.regouby@free.fr>
2024-05-11 12:19:31 +0200

    update FR .mo file

---
+++ tests/testthat/test-message-translations.R
@@ -0,0 +1,21 @@
+test_that("nn_conv1d padding_mode value_error() get translated in french", {
+  testthat::skip_on_ci()
+  testthat::skip_on_cran()
+  withr::with_language(lang = "fr",
+                       expect_error(
+                         nn_conv1d(3, 3, 2, 1, padding_mode = "bad_padding"),
+                         regexp = "‘padding_mode’ doit être pris parmi "
+                       )
+  )
+})
+
+test_that("runtime_error() get translated in french", {
+  testthat::skip_on_ci()
+  testthat::skip_on_cran()
+  withr::with_language(lang = "fr",
+                       expect_error(
+                         jit_tuple(c(7,3)),
+                         regexp = "L’argument ‘x’ doit être une liste."
+                       )
+  )
+})
