d27c4f96ab21124fce5fb49fc34bb84dd1db6480
Christophe REGOUBY <christophe.regouby@airbus.com>
2024-09-19 22:53:09 +0200

    can we revert 2d78c6b8 ?

--- tests/testthat/test-message-translations.R
+++ tests/testthat/test-message-translations.R
@@ -1,8 +1,4 @@
 test_that("R-level error messages are correctly translated in FR", {
-  # skip on cuda as image as no FR lang installed
-  skip_if(torch::cuda_is_available() && grepl("linux-gnu", R.version$os))
-  # skip on MAC M1
-  skip_if(torch::backends_mps_is_available() && R.version$arch == "aarch64")
   withr::with_language(lang = "fr",
                        expect_error(
                           torch_scalar_tensor(torch_randn(8, 2, 7)),
@@ -13,10 +9,6 @@ test_that("R-level error messages are correctly translated in FR", {
 })
 
 test_that("R-level warning messages are correctly translated in FR", {
-  # skip on cuda as image as no FR lang installed
-  skip_if(torch::cuda_is_available() && grepl("linux-gnu", R.version$os))
-  # skip on MAC M1
-  skip_if(torch::backends_mps_is_available() && R.version$arch == "aarch64")
   x <- torch_randn(2, requires_grad = TRUE)
   y <- torch_randn(1)
   b <- (x^y)$sum()
