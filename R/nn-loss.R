12447d608fca513efa7a4c8adc4b7fe9be971f7c
Sebastian Fischer <sebf.fischer@gmail.com>
2025-03-26 17:13:05 +0100

    feat: allow to pass R vector as loss weights (#1303)
    * feat: allow to pass R vector as loss weights
    
    * handle null case
    
    * avoid clone

--- R/nn-loss.R
+++ R/nn-loss.R
@@ -13,6 +13,9 @@ nn_weighted_loss <- nn_module(
   inherit = nn_loss,
   initialize = function(weight = NULL, reduction = "mean") {
     super$initialize(reduction)
+    if (!is.null(weight) && !inherits(weight, "torch_tensor")) {
+      weight = torch_tensor(weight)
+    }
     self$register_buffer("weight", weight)
   }
 )
