44b1a56fffaa483b6974394ddb2c9a8ec7dfeaf3
Daniel Falbel <dfalbel@gmail.com>
2026-04-16 15:37:01 -0300

    Remove dead generic dispatch code paths
    Delete call_c_function, do_call, .dispatch_cache, clean_names,
    make_cpp_function_name, cpp_clean_names, cpp_make_function_name,
    create_fn_name, and cpp_suffix. All generated code now uses direct calls,
    inline R dispatch, or C++ dispatchers.
    
    Replace torch:::cpp_make_function_name in torchgen with a pure-R
    implementation so codegen no longer depends on the removed C++ function.

--- R/codegen-utils.R
+++ R/codegen-utils.R
@@ -16,16 +16,6 @@ as_1_based_tensor <- function(x) {
   to_index_tensor(x)
 }
 
-clean_chars <- c("'", "\"", "%", "#", ":", ">", "<", ",", " ", "*", "&")
-
-clean_names <- function(x) {
-  cpp_clean_names(x, clean_chars)
-}
-
-make_cpp_function_name <- function(method_name, arg_types, type) {
-  cpp_make_function_name(method_name, names(arg_types), arg_types, type)
-}
-
 # Type-check helpers used by generated inline dispatch code.
 # These replicate the priority logic in cpp_arg_to_torch_type for specific
 # type pairs so that codegen can emit simple if/else branches.
@@ -37,28 +27,4 @@ is_tensor_dispatch <- function(x) {
 
 is_int64_dispatch <- function(x) {
   is.numeric(x) && length(x) == 1L
-}
-
-do_call <- function(fun, args) {
-  args_needed <- names(formals(fun))
-  args <- args[args_needed]
-  do.call(fun, args)
-}
-
-.dispatch_cache <- new.env(parent = emptyenv())
-
-call_c_function <- function(fun_name, args, expected_types, nd_args, return_types, fun_type) {
-  fun_name <- create_fn_name(fun_name, fun_type, nd_args, args, expected_types)
-
-  cached <- .dispatch_cache[[fun_name]]
-  if (is.null(cached)) {
-    f <- getNamespace("torch")[[fun_name]]
-    if (is.null(f)) {
-      value_error("{fun_name} does not exist")
-    }
-    cached <- list(f = f, args_needed = names(formals(f)))
-    .dispatch_cache[[fun_name]] <- cached
-  }
-
-  do.call(cached$f, args[cached$args_needed])
 }
